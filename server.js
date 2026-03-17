75b82b88f6ffac79adeb5064579dba09de514017
zeddicusgrey <mariouzor1@gmail.com>
2026-03-17 21:37:24 +0100

    Refactor comments and streamline server start message

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// server.js - fully compatible with Render
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve all files in public folder
+// Serve static files from public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Catch-all route: sends index.html for any request
+// Catch-all route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Arcane Empire server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Arcane Empire running on port ${PORT}`));
