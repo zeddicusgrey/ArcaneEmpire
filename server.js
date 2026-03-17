8a54a04b642825993259429e3f3e0b1273ff5607
zeddicusgrey <mariouzor1@gmail.com>
2026-03-17 20:18:03 +0100

    git add server.js git commit -m "Fix server to serve public folder" git push

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-// server.js - ArcaneEmpire fully working for Render
-
+// server.js - fully compatible with Render
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' folder
+// Serve all files in public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Catch-all route to send index.html for any request
+// Catch-all route: sends index.html for any request
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
+// Start server
 app.listen(PORT, () => {
   console.log(`Arcane Empire server running on port ${PORT}`);
 });
